29a93235254aa7297edb9aaace866adfb335dc86
Alex Vaslow <abv10@case.edu>
2022-10-17 15:36:43 -0400

    Transfrom code added

--- dicom_processor.py
+++ dicom_processor.py
@@ -1,28 +1,40 @@
+import os
+
 import pydicom as pyd
 import pydicom.data
 import csv
 from os.path import exists
+import dicom2nifti
 
 metadata_file = open("segmented_images/manifest-1605042674814/metadata.csv")
 csvreader = csv.reader(metadata_file)
 header = []
 header = next(csvreader)
-print(header)
 rows = []
 for row in csvreader:
     rows.append(row)
 loc_index = header.index("File Location")
+
 all_name = "1-1.dcm"
 for row in rows:
     original_path = row[loc_index]
-    new_path = original_path.replace(".\\", "segmented_images/manifest-1605042674814\\") + "\\" + all_name
+    new_path = original_path.replace(".\\", "segmented_images/manifest-1605042674814\\")  + "\\" + all_name
     ds = pyd.dcmread(new_path)
-    print(ds.pixel_array.sum())
-
-for image in ds.pixel_array:
-    print(image.shape)
-    print(image.sum())
+    print(ds.pixel_array.shape)
 
 
+metadata_file = open("original_images/manifest-1605042674814/metadata.csv")
+csvreader = csv.reader(metadata_file)
+header = []
+header = next(csvreader)
+rows = []
+for row in csvreader:
+    rows.append(row)
+loc_index = header.index("File Location")
+
+path = rows[0][loc_index].replace(".\\", "original_images/manifest-1605042674814\\")
+for file in os.listdir(path):
+    ds = pyd.dcmread(path +"\\" + file)
+    print(ds.pixel_array.shape)
 
 exit(-3)
---
+++ transforms.py
@@ -0,0 +1,78 @@
+import os
+import random
+
+import torch
+import numpy as np
+import torchio as tio
+
+
+def augment_images(image_path, label_path, task, new_id, transform):
+    original_image = tio.ScalarImage(image_path)
+    original_label = tio.LabelMap(label_path)
+
+    transformed_image = transform(original_image)
+    transformed_label = transform(original_label)
+
+    transformed_label.save("nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/labelsTr/PROS_" + str(new_id) + ".nii.gz")
+    transformed_image.save("nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/imagesTr/PROS_" + str(new_id) + ".nii.gz")
+
+
+def flip_images(task):
+    image_path = "nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/imagesTr/"
+    label_path = "nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/labelsTr/"
+
+    images = os.listdir(image_path)
+    labels = os.listdir(label_path)
+    max_id = len(images)
+    if max_id != len(labels):
+        print("EXCEPTION: NOT MATCHED DATASET and LABEL SIZE")
+    flip = tio.RandomFlip(flip_probability=1.0, axes=('LR',))
+    current_id = max_id + 1
+    for i in range(max_id):
+        or_im = image_path + images[i]
+        or_lab = label_path + labels[i]
+        augment_images(or_im, or_lab, task, pad(current_id), flip)
+        current_id += 1
+
+
+def affine_transform(task):
+    image_path = "nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/imagesTr/"
+    label_path = "nnUNET_raw_data_base/nnUNET_raw_data/" + task + "/labelsTr/"
+
+    images = os.listdir(image_path)
+    labels = os.listdir(label_path)
+    max_id = len(images)
+    if max_id != len(labels):
+        print("EXCEPTION: NOT MATCHED DATASET and LABEL SIZE")
+
+    current_id = max_id + 1
+    for i in range(max_id):
+        or_im = image_path + images[i]
+        or_lab = label_path + labels[i]
+
+        transform_one = get_transform()
+        augment_images(or_im, or_lab, task, pad(current_id), transform_one)
+        current_id += 1
+
+        transform_two = get_transform()
+        augment_images(or_im, or_lab, task, pad(current_id), transform_two)
+        current_id += 1
+
+
+def get_transform():
+    scale1 = random.uniform(0.8, 1.2)
+    scale2 = random.uniform((0.8, 1.2))
+    degrees = random.randint(-10, 10)
+    return tio.RandomAffine(
+        scales=(scale1, scale2),
+        degrees=degrees,
+    )
+
+
+def pad(id_num):
+    if id_num < 10:
+        return "00" + str(id_num)
+    elif id_num < 100:
+        return "0" + str(id_num)
+    else:
+        return str(id_num)
